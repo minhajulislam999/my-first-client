4f1d8c653c0771feaf0ee8dbd5962db3002c3082
minhajul islam <mm01882390860@gmail.com>
2026-04-03 13:10:38 +0600

    initial commit: first react client with own server

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <>
+     
+
+      {users.map(user => (
+      <p key={user.id}>{user.name}</p>
+    ))}
+
+     
+
+     
+    </>
+  )
+}
+
+export default App
